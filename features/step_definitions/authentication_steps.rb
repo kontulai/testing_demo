7eb2a17939c921af967541b891894b2fde17b148
Antti Relander <antti.relander@gmail.com>
2011-10-19 14:36:41 +0300

    all relevant cucumber tests implemented for existing features, begin to implement new features

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -3,11 +3,18 @@ Given /^The user "user@example.com" has an account$/ do
   @user = User.create!(:name => 'First Name', :password => 'please', :email => "user@example.com")
 end
 
+module LoginSteps
+  def login
+    visit('/users/sign_in')
+    fill_in('user_email', :with => @user.email)
+    fill_in('user_password', :with => @user.password)
+    click_button('Sign in')
+  end
+end
+World(LoginSteps)
+
 When /^he logs in$/ do
-  visit('/users/sign_in')
-  fill_in('user_email', :with => @user.email)
-  fill_in('user_password', :with => @user.password)
-  click_button('Sign in')
+  login()
 end
 
 Then /^he should see text "([^"]*)"$/ do |username|
@@ -31,7 +38,7 @@ end
 
 Then /^I should see "([^"]*)"$/ do |not_logged_in|
   page.should have_content(not_logged_in)
-  #refactor to log in before logging out
+  #refactor to log in before logging out - need to put this to another feature with login background
   #page.should have_content('Signed out successfully.')
 end
 
