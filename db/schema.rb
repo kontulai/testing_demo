4a0d3ac06db906c8d676aaa04ca167de9ac22463
Antti Relander <antti.relander@gmail.com>
2011-10-19 15:07:48 +0300

    added scenarios for new features

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111018120401) do
+ActiveRecord::Schema.define(:version => 20111019115428) do
+
+  create_table "reservations", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
