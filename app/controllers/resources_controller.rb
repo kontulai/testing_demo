46e24a0f8c4446de9d306f2bfcf6cd4dd4c8382e
Antti Relander <antti.relander@gmail.com>
2011-10-20 13:14:52 +0300

    reservation view works ok, with test data - todo: add more test data and make better ui

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,6 +1,8 @@
 class ResourcesController < ApplicationController
   def show
      @resource = Resource.find(params[:id])
+     #@reservation = Reservation.where("resource_id = "+params[:id])
+     @reservations = Reservation.all
   end
 
 end
